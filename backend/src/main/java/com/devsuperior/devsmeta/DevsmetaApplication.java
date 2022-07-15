c0c62d8cc48b8af7af06085452d9a92e7077d320
Irthyell Muniz <irthyellnaua@gmail.com>
2022-07-15 19:17:43 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/devsmeta/DevsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/devsmeta/DevsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
